c26b520ad67168c2458273742ae8f43d522f8922
tsider <tsider@s26.etu.info-ufr.univ-montp2.fr>
2013-09-19 10:15:19 +0200

    classe java

---
+++ HelloMain.java
@@ -0,0 +1,10 @@
+public class HelloMain {
+
+	public static void main(String[] args) {
+
+	HelloWord N= new HelloWord("  tak");
+	N.print(); 
+	}
+
+}
+
---
+++ HelloWord.java
@@ -0,0 +1,28 @@
+
+
+public class HelloWord {
+
+	private String name;
+	
+	//public helloword(){}
+	public HelloWord (String name){
+	
+	this.name=name;
+
+	}
+	
+	public String getname() {
+		return name;
+	}
+
+	public void setNom(String name) {
+		this.name = name;
+	}
+
+	public void print(){
+	//String affi= "Helloword"+this.name;
+	System.out.println("Helloword   "+"  "+this.name);
+	}
+}
+	
+
